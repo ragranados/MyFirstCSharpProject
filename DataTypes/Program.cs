979750b3eb74742478d983e85328a66a522c6ab6
ragranados <raul.granados12@gmail.com>
2024-06-08 01:01:35 -0600

    20:33

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -10,6 +10,18 @@ namespace DataTypes
     {
         static void Main(string[] args)
         {
+            int age = 25; //caso 26 :D
+
+            Console.WriteLine(age);
+
+            long bigNumber = 90000000L; //para numerlos largos se agrega la L
+            Console.WriteLine(bigNumber);
+
+            double negative = -55.2D; //la D es para asegurarse que es double
+
+            float precision = 5.0000001F; //F para float
+
+            decimal money = 14.99M; // para decimal que se usa para dinero
 
             Console.ReadLine();
         }
---
+++ Strings_Chars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Hola";
+            char letter = 'a'; //caracter por defecto '\0'
+
+            Console.WriteLine(name);
+            Console.WriteLine(letter);
+
+            Console.ReadLine();
+        }
+    }
+}

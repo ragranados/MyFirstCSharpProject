a4af42ae2fa10e91fee3392371e7ad4d92260540
Raúl Granados <raul.granados12@gmail.com>
2024-06-09 01:29:46 -0600

    1:29:25

---
+++ For/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i = 0; i < 10; i += 2)
+            {
+                Console.WriteLine("Hi " + i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Switch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Day of the week: ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            switch (day) 
+            {
+                case 1:
+                    Console.WriteLine("Lunes");
+                    break;
+                case 2: 
+                    Console.WriteLine("Martes");
+                    break;
+                default:
+                    Console.WriteLine("Ninguno");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}

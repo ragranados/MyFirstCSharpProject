69e3023cc3b00cc489f02723cd2406b1713389b7
ragranados <raul.granados12@gmail.com>
2024-06-11 14:45:52 -0600

    2:14:59

--- Exercise 1/Program.cs
+++ Exercise 1/Program.cs
@@ -10,6 +10,39 @@ namespace Exercise_1
     {
         static void Main(string[] args)
         {
+
+            //Console.Write("Escribe un numero: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("{0} x {1} = {2}", i, number, i * number); ;
+            //}
+
+            int number = 15;
+
+            for (int i = 1; i <= 15; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ Output/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //en este caso la arroba es para decir "lo que se ve ahi, es lo que se usa"
+            //el $ es para lo mismo que en JS, parecido
+            string prueba = @"prueba ""raul""";
+
+            Console.WriteLine(@"C:\Users\raul\n");
+            Console.WriteLine($"{prueba} esto es asi: {145}");
+
+            Console.ReadLine();
+        }
+    }
+}

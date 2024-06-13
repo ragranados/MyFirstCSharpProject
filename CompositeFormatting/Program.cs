5b12d4b280fc3ed1a01d72d1a5d9a3b99262645c
ragranados <raul.granados12@gmail.com>
2024-06-13 17:04:22 -0600

    2:17:21

---
+++ CompositeFormatting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositeFormatting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aba";
+            //int age
+        }
+    }
+}

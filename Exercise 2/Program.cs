2bb63892bb972ca7fb2fa226079a3847219f300b
ragranados <raul.granados12@gmail.com>
2024-06-16 19:40:47 -0600

    3:10:08

--- Exercise 2/Program.cs
+++ Exercise 2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Exercise_2
 {
@@ -11,7 +12,21 @@ namespace Exercise_2
         static void Main(string[] args)
         {
 
+            Console.Write("Escribe un mensaje: ");
+            string mensaje = Console.ReadLine();
 
+            for (int i = 0; i < mensaje.Length; i++)
+            {
+                Console.Write(mensaje[i]);
+                Thread.Sleep(100);
+            }
+
+            //el reverso se empieza en menos un porque lo de los arreglos
+            for (int i = mensaje.Length - 1; i >= 0; i--)
+            {
+                Console.Write(mensaje[i]);
+                Thread.Sleep(100);
+            }
 
             Console.ReadLine();
         }
---
+++ Exercise 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese contraseña: ");
+            string contra = Console.ReadLine();
+
+            Console.Write("Ingrese contraseña: ");
+            string contraC = Console.ReadLine();
+
+            /*
+                Si se quiere comprar, sin tener en cuentra las mayusculas y 
+                minusculas se usa ToLower y ToUpper de string
+            */
+
+            if (contra.Equals(string.Empty) || contraC.Equals(string.Empty))
+            {
+                Console.WriteLine("Vacio");
+            }
+            else
+            {
+                if (contra.Equals(contraC))
+                {
+                    Console.WriteLine("Si son iguales");
+                }
+                else
+                {
+                    Console.WriteLine("No son iguales");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

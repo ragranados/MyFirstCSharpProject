b794f2c0c9ea73b08584440437c07b9e927a3ad2
ragranados <raul.granados12@gmail.com>
2024-06-19 10:36:54 -0600

    5:46:12

---
+++ Ref/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ref
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            Assign(ref num);
+            Console.WriteLine(num);
+
+            Console.ReadLine();
+        }
+
+        /*
+        La diferencia es que out si hay que asignar por obligacion y ref no
+        ref es como dar un "link" a la variable de afuera
+
+        Tambien para ref, siempre hay que declarar la variable antes, por ser un
+        "link" hacia la variable
+
+        ref es un poco mas rapido
+         */
+        static void Assign(ref int num)
+        {
+            num = 10;
+        }
+    }
+}

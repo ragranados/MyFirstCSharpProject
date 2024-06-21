462d49dd99b2c253e5220f0103a277cdfbfd15c5
ragranados <raul.granados12@gmail.com>
2024-06-20 23:53:41 -0600

    7:26:51

---
+++ Classes/ClassesTry/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Person
+    {
+
+        public string name;
+        public int age;
+
+        public Person() { }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string ReturnDetails(string extra)
+        {
+            return $"Nombre: {this.name}, edad: {this.age}, {extra}";
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,6 +10,20 @@ namespace Classes
     {
         static void Main(string[] args)
         {
+
+            Person person = new Person("Raul", 26);
+            //person.age = 27;
+            //person.name = "Raul 2";
+
+            Console.WriteLine(person.ReturnDetails("Contenido extra"));
+            ReturnDetails(person);
+
+            Console.ReadLine();
+        }
+
+        static void ReturnDetails(Person person)
+        {
+            Console.WriteLine($"{person.name}");
         }
     }
 }

5e9a18dd92060288c960113f992a5b0d6ee320f5
ragranados <raul.granados12@gmail.com>
2024-06-20 23:55:22 -0600

    7:26:51

--- Classes/ClassesTry/Person.cs
+++ Classes/ClassesTry/Person.cs
@@ -8,11 +8,11 @@ namespace Classes
 {
     class Person
     {
-
         public string name;
         public int age;
 
         public Person() { }
+
         public Person(string name, int age)
         {
             this.name = name;

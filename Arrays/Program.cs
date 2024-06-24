20cc1651cbca55b6845b1efa1952f16995138fd5
Raúl Granados <raul.granados12@gmail.com>
2024-06-24 15:08:31 -0600

    99

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -85,6 +85,36 @@ namespace Arrays
             //los siguientes argumentos son para el rango de busqueda.
             Console.WriteLine(Array.IndexOf(numeros_indexof, 733));
 
+            //copiar arreglos
+            int[] numeros_copiar = new int[]
+                {
+                    0,1,2,3,4,5,6
+                };
+
+            int[] copia_numeros = new int[numeros_copiar.Length];
+
+            //Array.Copy(numeros_copiar, copia_numeros, numeros_copiar.Length);
+
+            Array.Copy(numeros_copiar, 2,copia_numeros,0, 5);
+
+            //copia_numeros = numeros_copiar;
+
+            //numeros_copiar[0] = 256;
+
+            //si un arreglo se asigna a otro normalmente, se le asigna la memoria, por eso existe la funcion para copiar
+
+            foreach (var item in numeros_copiar)
+            {
+                Console.Write(item);
+            }
+
+            Console.WriteLine(Environment.NewLine);
+
+            foreach (var item in copia_numeros)
+            {
+                Console.Write(item);
+            }
+
             Console.ReadLine();
         }
     }
---
+++ Arrays_Multidimensional/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_Multidimensional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] board = new int[3, 3];
+            board[0, 0] = 5;
+
+            int[,] board2 = new int[3, 3]
+            {
+                { 1,2,3},
+                { 4,5,6},
+                { 7,8,9 }
+            };
+
+            int[,,] chessBoard = new int[3, 3, 3]
+                {
+                    {
+                { 1,2,3},
+                { 4,5,6},
+                { 7,8,9 }
+            },
+                    {
+                { 1,2,3},
+                { 4,5,6},
+                { 7,8,9 }
+            },
+                    {
+                { 1,2,3},
+                { 4,5,6},
+                { 7,8,9 }
+            }
+                };
+
+            //en el caso de los arreglos multidimensionales, el usar Length, te devuele el numero total de elementos que hay en el arreglo
+
+            //for (int i = 0; i < board2.GetLength(0); i++)//0 1 2
+            //{
+            //    for (int j = 0; j < board2.GetLength(1); j++) 
+            //    {
+            //        Console.Write(board2[i, j]);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            foreach (var item in board2)
+            {
+                Console.Write(item);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}

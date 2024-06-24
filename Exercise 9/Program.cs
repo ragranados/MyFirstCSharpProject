70108b92b19a615534ded70d66e2d69963aacbf3
ragranados <raul.granados12@gmail.com>
2024-06-24 12:12:05 -0600

    94

---
+++ Exercise 9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un mensaje: ");
+            string mensaje = Console.ReadLine();
+            //StringBuilder mensaje = new StringBuilder(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+
+            //Console.WriteLine(mensaje);
+
+            foreach (var i in mensaje)
+            {
+                sb.Append(char.IsLower(i) ? char.ToUpper(i) : char.ToLower(i));
+            }
+
+            Console.Write(sb.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringBuilder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine("Hello ");
+            stringBuilder.Append("World");
+            stringBuilder.Insert(stringBuilder.Length,"Hi", 5);
+            stringBuilder.Replace("Hi", "Equis de");
+            stringBuilder.Remove(0,8);
+
+            Console.WriteLine(stringBuilder.ToString());
+
+            string mensaje = "";
+            mensaje += "Hola" + Environment.NewLine;
+            mensaje += "Adios";
+
+            Console.WriteLine(mensaje);
+
+            Console.ReadLine();
+        }
+    }
+}

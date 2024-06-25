bde67aafcbd394b313adc7a347e74834f81be31a
Raúl Granados <raul.granados12@gmail.com>
2024-06-25 15:14:02 -0600

    127

---
+++ Exercise 10/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = new List<string>()
+            {
+                "Item 1", "Item 2", "Item 3"
+            };
+
+            Console.Write("Ingrese un nuevo item: ");
+            items.Add(Console.ReadLine());
+
+            foreach (string item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.Write("Item a actualizar: ");
+
+            int indexActualizar = items.IndexOf(Console.ReadLine());
+
+            if (indexActualizar >= 0)
+            {
+                Console.Write("Nuevo Item: ");
+                items[indexActualizar] = Console.ReadLine(); ;
+            }
+            else
+            {
+                Console.WriteLine("No encontrado");
+            }
+
+            foreach (string item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+    }
+}
---
+++ Split/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string comma = "Raul,26,1";
+
+            string[] commas = comma.Split(',');
+
+            foreach (var item in commas)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(string.Join(" ",commas));
+
+            Console.ReadLine();
+        }
+    }
+}

293d5e0acfd53edc62f50395283b85b5cdb05c84
ragranados <raul.granados12@gmail.com>
2024-06-25 22:18:39 -0600

    172

---
+++ CallStack/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallStack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintDetails();
+            Convert.ToInt32("Hola");
+
+            Console.ReadLine();
+        }
+
+        static void PrintDetails()
+        {
+            Console.WriteLine("Prueba");
+        }
+    }
+}
--- Debugging/Program.cs
+++ Debugging/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Debugging
 {
@@ -13,7 +14,10 @@ namespace Debugging
 
             DebuggingTest();
 
-            //Debug.WriteLine();
+            Debug.WriteLine("Prueba");
+            Debug.WriteLineIf(true, "Prueba If");
+            //Debug.Assert(false, "Prueba");
+            Console.WriteLine();
 
             Console.ReadLine();
         }

fe1a4e2c2e8ccb3269959ddc8198d714826486e5
ragranados <raul.granados12@gmail.com>
2024-06-27 22:49:09 -0600

    189

---
+++ Protected/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protected
+{
+    internal class Program
+    {
+
+        //protected, si es privado, la clase que herede de ella no lo podra usar, pero si es protectec y privado, la clase que herede de ella si podra hacer uso de ese campo
+        class Point
+        {
+            private protected int x;
+            private int y;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        class Position : Point
+        {
+            int z;
+
+            public Position(int x, int y, int z) : base(x, y)
+            {
+                this.z = z;
+                this.x = z;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Point point = new Point(2, 5);
+
+            Position position = new Position(4, 5, 6);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sealed/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealed
+{
+
+    //No se puede heredar de una clase "sealed"
+    //Tambien se puede usar en funciones
+    //Sirve para hacer optimizaciones porque el compilador sabra que esas funciones no se van a usar al heredar o esas clases
+    // no se puede tener un public sealed sin override
+
+    class A { }
+
+    sealed class B : A { }
+
+    //class C : B { }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
